080c70991877904dc4dbfe019e2d6c43068a6a83
ayyastakim <takimayyas@gmail.com>
2025-12-24 09:44:29 +0800

    feat(basic): add simple introduction syntax

---
+++ src/basictypes/HelloWorld.kt
@@ -0,0 +1,5 @@
+package basictypes
+
+fun main() {
+    println("hello world!")
+}
---
+++ src/basictypes/StringTemplates.kt
@@ -0,0 +1,8 @@
+package basictypes
+
+fun main() {
+    val customers = 10
+    println("there are $customers customers")
+
+    println("there are ${customers + 1} customers")
+}
